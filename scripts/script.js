2ba0cff53be41d2eecfabdb60369bd98cea27c8b
Farhan Tanvir <farhantanvir200000@gmail.com>
2025-02-17 23:57:00 +0600

    (feat) I have added some function for displaying hour, minute & seconds

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,37 @@
 function toogleMenu() {
   const header = document.getElementById("header");
   header.style.height = (header.style.height === "60px") ? "210px" : "60px"
-  // if (header.style.height === "60px") {
-  //   header.style.height = "210px"
-  // } else {
-  //   header.style.height = "60px"
-  // }
+}
+const seconds = 1000
+const minute = seconds * 60;
+const hour = minute * 60;
+function displayTime() {
+  const date = new Date();
+  const displayHour = document.querySelector("#hour");
+  const displayMinute = document.querySelector("#minute");
+  const displaySecond = document.querySelector("#second");
+  const am = document.querySelector('#am');
+  const pm = document.querySelector('#pm');
+  let hours;
+if (date.getHours() > 12) {
+  hours = date.getHours() - 12;
+  pm.style.backgroundColor = "mediumseagreen";
+  pm.style.color = "#fff";
+} else {
+  hours = date.getHours();
+  am.style.backgroundColor = "mediumseagreen";
+  am.style.color = "#fff";
 }
 
-
+ displayHour.value = hours.toString().padStart(2,"0");
+ displayMinute.value = date.getMinutes().toString().padStart(2,"0");
+ displaySecond.value = date.getSeconds().toString().padStart(2,"0");
+}
 
 function init() {
   console.log("Javascript initialized.")
-  toogleMenu()
+  toogleMenu();
+  displayTime();
+  setInterval(displayTime,1000)
 };
 init()
