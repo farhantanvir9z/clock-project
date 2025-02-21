813a48bcb09618787b15741c938805ffcdbb6ffd
Farhan Tanvir <farhantanvir200000@gmail.com>
2025-02-21 15:42:56 +0600

    (feat) add functions for displaying dates

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+let date;
 function toogleMenu() {
   const header = document.getElementById("header");
   header.style.height = (header.style.height === "60px") ? "210px" : "60px"
@@ -12,11 +13,15 @@ function displayTime() {
   const displaySecond = document.querySelector("#second");
   const am = document.querySelector('#am');
   const pm = document.querySelector('#pm');
+  pm.style.backgroundColor = "";
+  pm.style.color = "";
+  am.style.backgroundColor = "";
+  am.style.color = ""
   let hours;
 if (date.getHours() > 12) {
   hours = date.getHours() - 12;
   pm.style.backgroundColor = "mediumseagreen";
-  pm.style.color = "#fff";
+  pm.style.color = "#fff"; 
 } else {
   hours = date.getHours();
   am.style.backgroundColor = "mediumseagreen";
@@ -27,11 +32,18 @@ if (date.getHours() > 12) {
  displayMinute.value = date.getMinutes().toString().padStart(2,"0");
  displaySecond.value = date.getSeconds().toString().padStart(2,"0");
 }
-
+function displayDate() {
+  date = new Date();
+  const displayDate = document.querySelector('#displayDate');
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  displayDate.innerHTML = `${date.getDate()} ${months[date.getMonth()]}, ${date.getFullYear()}`
+}
 function init() {
   console.log("Javascript initialized.")
   toogleMenu();
   displayTime();
   setInterval(displayTime,1000)
+  displayDate()
+  setInterval(displayDate,1000)
 };
 init()
